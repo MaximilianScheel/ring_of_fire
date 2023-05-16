16ac8496bfb13cd714f2a9cf0409f994ec22ecad
Maximilian Scheel <scheel.development@gmail.com>
2023-05-16 17:59:09 +0200

    upload new project

---
+++ src/app/player/player.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-player',
+  templateUrl: './player.component.html',
+  styleUrls: ['./player.component.scss']
+})
+export class PlayerComponent {
+
+  @Input() name;
+  @Input() playerActive: boolean = false;
+
+}
+
+
